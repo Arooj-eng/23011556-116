14d2fa415cdff18d29795cbced56eff795a3d839
Arooj-eng <aroojzara222@gmail.com>
2025-11-04 00:19:47 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assignment
+namespace Assignment2
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("\nRunning EvenOdd:");
-            EvenOdd.evenodd();
-
-            Console.WriteLine("\nRunning Calculator:");
-            Calculator.calculate();
-
-            Console.WriteLine("\nRunning Grade:");
-            grade.Marks();
-
-            Console.WriteLine("\nRunning SumOfNaturalNumbers:");
-            SumofNaturalNo.Sum();
-
-            Console.WriteLine("\nRunning MultiplicationTable:");
-            MultiplicationTable.Table();
-
-            Console.WriteLine("\nRunning Factorial:");
-            factorial.Fact();
-
-            Console.WriteLine("\nRunning Reverse:");
-            Reverse.Revno();
-            Console.WriteLine("\nRunning ArrayMinMax:");
-            ArrayMinMax.MinMax();
-
-            Console.WriteLine("\nRunning CountEvenOdd:");
-            CountEvenOdd.Counting();
-
-            Console.WriteLine("\nRunning Search:");
-            Search.check();
-
-
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
         }
     }
 }
